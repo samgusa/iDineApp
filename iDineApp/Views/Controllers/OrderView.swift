a3d01574f964d0c61d60a0283a2e7b951af70abf
= <samgusaapp1993@gmail.com>
2021-07-08 21:10:57 -0400

    Finish adding items to an order with EnvironmentObject. Created an OrderView, which is where we will place orders.

---
+++ iDineApp/Views/Controllers/OrderView.swift
@@ -0,0 +1,43 @@
+//
+//  OrderView.swift
+//  iDineApp
+//
+//  Created by Sam Greenhill on 7/8/21.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                Section {
+                    NavigationLink(
+                        destination: Text("Check out")) {
+                        Text("Place Order")
+                    }
+                }
+            }
+            .navigationTitle("Order")
+            .listStyle(InsetGroupedListStyle())
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView().environmentObject(Order())
+    }
+}
